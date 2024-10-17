f791ed36af8fcbe588c9eb35b1563135f54752fc
quickbreak <michaelzapryagaev@gmail.com>
2024-10-17 15:41:53 +0300

    change Boost.test to Gtest!

--- test_version.cpp
+++ test_version.cpp
@@ -1,14 +1,14 @@
-#define BOOST_TEST_MODULE test_version
-
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
+TEST(test_case_name, test_name)
 {
-    BOOST_CHECK(version() > 0);
+	ASSERT_TRUE(version() > 0);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
